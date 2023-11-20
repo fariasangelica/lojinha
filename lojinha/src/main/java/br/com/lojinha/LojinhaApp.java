b17d248dca2a684166eb2aaa3d890aa0c094e058
angelica 123 <hendhiooo@gmail.com>
2023-11-20 17:32:08 -0300

    novo arquivo

--- lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
+++ lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,25 +1,39 @@
 package br.com.lojinha;
 
+import br.com.lojinha.enums.Tamanho;
 import br.com.lojinha.pojo.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LojinhaApp {
     public static void main(String[] args) {
 
         /* para utilizar uma classe através de outra classe é necessário importa-la */
 
-        Produto meuProduto = new Produto ();
+        Produto meuProduto = new Produto("Nestle", Tamanho.MEDIO);
 
         /* valores aos atributos */
 
-        meuProduto.nome = "Play Station 4";
-        meuProduto.valor = 4999.99;
-        meuProduto.marca = "Sony";
-        meuProduto.tamanho = "Medio";
-        meuProduto.itensInclusos = "2 Controles e 3 Jogos";
+        meuProduto.setNome("Play Station 4");
+        meuProduto.setValor(30);
 
-        System.out.println(meuProduto.nome);
+        meuProduto.setTamanho(Tamanho.PEQUENO);
 
+        List<String> itensInclusos = new ArrayList<>(); // lista//
+        itensInclusos.add("2 Controles");
+        itensInclusos.add("3 Jogos");
+        itensInclusos.add("2 cabos de energia");
+        meuProduto.setItensInclusos(itensInclusos);
+
+        System.out.println(meuProduto.getItensInclusos().get(1));
+        System.out.println(meuProduto.getTamanho());
+
+        System.out.println(meuProduto.getValor());
+        System.out.println(meuProduto.getMarca());
 
 
     }
+
 }
+
